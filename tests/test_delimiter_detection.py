d0d5cfb048fdea65d0fbb340ae64cfa82a32de5e
George Burton <8233643+gecBurton@users.noreply.github.com>
2023-12-30 17:00:39 +0000

    simplified class structure (#26)
    * simplified class structure
    
    * bumped version
    
    * check py versions 3.9, 3.10, 3.11
    
    ---------
    
    Co-authored-by: George Burton <g.e.c.cburton@gmail.com>

--- tests/test_delimiter_detection.py
+++ tests/test_delimiter_detection.py
@@ -1,22 +1,22 @@
 import pytest
 
-from csv_bleach.detect_delimiter import DelimiterDetector
+from csv_bleach.detect_delimiter import DelimiterDetector, combine
 
 
 @pytest.mark.parametrize(
     "txt, result",
     [
         (
-            'a,b|c,e;;f g,"i"|, k',
+            b'a,b|c,e;;f g,"i"|, k',
             DelimiterDetector({" ": 2, ",": 4, ";": 2, "|": 2}),
         ),
-        ("a b c   d", DelimiterDetector({" ": 3})),
+        (b"a b c   d", DelimiterDetector({" ": 3})),
         (
-            '"Joan ""the bone"", Anne",Jet,"9th, at Terrace plc",Desert City,CO,00123',
+            b'"Joan ""the bone"", Anne",Jet,"9th, at Terrace plc",Desert City,CO,00123',
             DelimiterDetector({",": 5, " ": 1}),
         ),
         (
-            "a   b   c",
+            b"a   b   c",
             DelimiterDetector({" ": 2}),
         ),
     ],
@@ -27,7 +27,7 @@ def test_parse(txt, result):
 
 def test_parse_fails():
     with pytest.raises(ValueError) as type_error:
-        DelimiterDetector.parse_row('a, "b, c')
+        DelimiterDetector.parse_row(b'a, "b, c')
     assert type_error.value.args == ('row not escaped: `a, "b, c`',)
 
 
@@ -37,7 +37,7 @@ def test_combine_1():
         DelimiterDetector({",": 3, ";": 2, "|": 12}),
         DelimiterDetector({",": 3, "|": 1}),
     ]
-    assert DelimiterDetector.combine(iter(items)) == DelimiterDetector({",": 3})
+    assert combine(iter(items)) == DelimiterDetector({",": 3})
 
 
 def test_combine_2():
@@ -45,7 +45,7 @@ def test_combine_2():
         DelimiterDetector({",": 3, " ": 2}),
         DelimiterDetector({",": 3, " ": 2}),
     ]
-    assert DelimiterDetector.combine(iter(items)) == DelimiterDetector({",": 3})
+    assert combine(iter(items)) == DelimiterDetector({",": 3})
 
 
 def test_combine_fail():
@@ -54,5 +54,5 @@ def test_combine_fail():
         DelimiterDetector({"|": 3}),
     ]
     with pytest.raises(ValueError) as value_error:
-        DelimiterDetector.combine(iter(items))
+        combine(iter(items))
     assert value_error.value.args == ("no delimiter detected in file",)
